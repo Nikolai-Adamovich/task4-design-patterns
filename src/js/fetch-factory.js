ec1b9ff15e73aa3b33f17128b9742c429c6b1b09
Nikolai Adamovich <adamovich.nikolai@gmail.com>
2018-12-05 23:35:23 +0300

    created Factory which creates GET and POST method based on fetch() function

--- src/js/fetch-factory.js
+++ src/js/fetch-factory.js
@@ -1,15 +1,25 @@
-const createQuery = (field, value, i) => {
+const createQuery = ([field, value], i) => {
   return `${i === 0 ? '?' : '&'}${field}=${encodeURIComponent(value)}`;
 };
 
 const fetchFactory = (method) => {
   switch (method) {
-    case 'get' : return (url, params) => {
-      let i = 0;
-      for (const param in params) {
-        //
-      }
-      fetch();
+    case 'get': return async (baseUrl, params) => {
+      let queryUrl = `${baseUrl}?${params.toString()}`;
+      const res = await fetch(queryUrl);
+      return res.json();
+    }
+    
+    case 'post': return async (baseUrl, params) => {
+      const headers = new Headers({
+        'x-api-key': params.get('apiKey')
+      });
+      const res = await fetch(baseUrl, {
+        method: 'POST',
+        body: params,
+        headers: headers
+      });
+      return res.json();
     }
   }
 };
--- src/js/get-news.js
+++ src/js/get-news.js
@@ -1,51 +1,53 @@
 import '../scss/components/_news.scss';
 import {mainParameters, paginationParameters} from './url-parameters';
+import fetchFactory from './fetch-factory';
 
-const baseUrl = 'https://newsapi.org/v2/';
+const fetchGet = fetchFactory('get');
+// post works, but it's not allowed to use on https://newsapi.org
+const fetchPost = fetchFactory('post');
+
+let baseUrl;
 const apiKey = 'd72c9d0925a44fb8a00c4f2ed855167c';
 
-const createQueryUrl = () => {
+const createQueryParams = () => {
+  baseUrl = 'https://newsapi.org/v2/';
   let parameterIndex = 0;
-  let queryUrl = baseUrl;
+  const queryParams = new URLSearchParams();
 
   // Add user search query if it is specified otherwise add
   if (/[а-яё\w\d]{3,}/i.test(mainParameters.get('q'))) {
-    queryUrl += 'everything';
+    baseUrl += 'everything';
   } else {
-    queryUrl += 'top-headlines';
+    baseUrl += 'top-headlines';
   }
   
   for (let parameter of mainParameters) {
     if (parameter[1]) {
-      queryUrl += createQuery(parameter, parameterIndex++);
+      queryParams.set(parameter[0], parameter[1]);
+      parameterIndex++;
     }
   }
 
   // Fallback in case if none of the required parameters are specified
   if (parameterIndex === 0) {
-    queryUrl += createQuery(['language', 'en'], parameterIndex++);
+    queryParams.set('language', 'en');
   }
 
   // Pagination parameters
   for (let parameter of paginationParameters) {
     if (parameter[1]) {
-      queryUrl += createQuery(parameter, parameterIndex++);
+      queryParams.set(parameter[0], parameter[1]);
     }
   }
 
-  queryUrl += createQuery(['apiKey', apiKey], parameterIndex++);
+  queryParams.set('apiKey', apiKey);
 
-  return queryUrl;
-};
-
-const createQuery = ([field, value], i) => {
-  return `${i === 0 ? '?' : '&'}${field}=${encodeURIComponent(value)}`;
+  return queryParams;
 };
 
 const getNews = async () => {
-  const queryUrl = createQueryUrl();
-  const res = await fetch(queryUrl);
-  return res.json();
+  const queryParams = createQueryParams();
+  return await fetchGet(baseUrl, queryParams);
 };
 
 export default getNews;
